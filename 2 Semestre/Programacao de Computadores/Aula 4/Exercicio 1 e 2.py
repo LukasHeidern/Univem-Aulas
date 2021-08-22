e272ce6b7ae6fd1f2423216ca2378b5c15065328
LukasHeidern <75574343+LukasHeidern@users.noreply.github.com>
2021-08-22 14:07:45 -0300

    Create Exercicio 1 e 2.py

---
+++ 2 Semestre/Programacao de Computadores/Aula 4/Exercicio 1 e 2.py
@@ -0,0 +1,10 @@
+'''1.Faça um programa em Python que leia uma palavra pelo teclado e faça a impressão 
+conforme o exemplo a seguir para a palavra AMOR:
+A
+AM
+AMO
+AMOR'''
+
+def ex01():
+    string = input("Digite uma palavra: ")
+    for i in range(len(string)+1): print(string[0:i])
