fb986547a98cdab3e3bf852f844eab9413c2a4ab
LukasHeidern <75574343+LukasHeidern@users.noreply.github.com>
2021-09-04 20:45:28 -0300

    lll

--- 2 Semestre/Sistemas Embarcados (C)/Aula 6/Lucas Ferrari - Exercicio.c
+++ 2 Semestre/Sistemas Embarcados (C)/Aula 6/Lucas Ferrari - Exercicio.c
@@ -21,48 +21,47 @@ int main(){
         printf("\nDigite o codigo do item que deseja comprar ou digite 0 para encerrar: ");
         scanf("%i",&C_odigo);
 		
-		if (C_odigo != 0){
+		if (C_odigo >= 100 && C_odigo <= 106){
 	        
 			printf("Digite a quantidaede: ");
 	        scanf("%i",&Q_uantidade);
-	        
-	        printf("Voce adicionou:\nEspecificacao      Codigo      Preco\n");
-	        
+	          
 	        if (C_odigo == 100){
-	            P_total = P_total + Q_uantidade * 1.20; 
+	            P_total += Q_uantidade * 1.20; 
 	            printf("Cachorro Quente    100         1,20 x %i\nTotal - %0.2f",Q_uantidade, 1.20 * Q_uantidade);}
 	
 	        else if (C_odigo == 101){
-	            P_total = P_total + Q_uantidade * 1.30;
+	            P_total += Q_uantidade * 1.30;
 	            printf("Bauru Simples      101         1,30 x %i\nTotal - %0.2f",Q_uantidade, 1.30 * Q_uantidade);}
 	
 	        else if (C_odigo == 102){
-	            P_total = P_total + Q_uantidade * 1.50;
+	            P_total += Q_uantidade * 1.50;
 	            printf("Bauru com Ovo      102         1,50 x %i\nTotal - %0.2f",Q_uantidade, 1.50 * Q_uantidade);}
 	
 	        else if (C_odigo == 103){
-	            P_total = P_total + Q_uantidade * 1.20;
+	            P_total += Q_uantidade * 1.20;
 	            printf("Hamburguer         103         1,20 x %i\nTotal - %0.2f",Q_uantidade, 1.20 * Q_uantidade);}
 	
 	        else if (C_odigo == 104){
-	            P_total = P_total + Q_uantidade * 1.70;
+	            P_total += Q_uantidade * 1.70;
 	            printf("Cheeseburger       104         1,70 x %i\nTotal - %0.2f",Q_uantidade, 1.70 * Q_uantidade);}
 	
 	        else if (C_odigo == 105){
-	            P_total = P_total + Q_uantidade * 1.20;
+	            P_total += Q_uantidade * 1.20;
 	            printf("Suco               105         2,20 x %i\nTotal - %0.2f",Q_uantidade, 2.20 * Q_uantidade);}
 	
 	        else if (C_odigo == 106){
-	            P_total = P_total + Q_uantidade * 1.00;
+	            P_total += Q_uantidade * 1.00;
 	            printf("Refrigerante       106         1,00 x %i\nTotal - %0.2f",Q_uantidade, 1.00 * Q_uantidade);}
 	        }
         
     	else if(C_odigo == 0){break;}
     	
     	else{printf("Codigo Invalido!");}
+	printf("O valor total da compra e %0.2f",P_total);
 		
     }
 		
     
-    printf("O valor total da compra e %0.2f",P_total);
+    
 }
