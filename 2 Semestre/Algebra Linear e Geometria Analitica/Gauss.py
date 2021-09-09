e176389167e51c02b99aa7863515d9435e660014
LukasHeidern <75574343+LukasHeidern@users.noreply.github.com>
2021-09-09 01:33:06 -0300

    Update Gauss.py

--- 2 Semestre/Algebra Linear e Geometria Analitica/Gauss.py
+++ 2 Semestre/Algebra Linear e Geometria Analitica/Gauss.py
@@ -1,11 +1,17 @@
+# def solution(matrix):
+
+
 def gauss_t(matrix):
-    for p in range(len(matrix)-1):
-        for l in range(1+p,len(matrix)):
+    for p in range(len(matrix) - 1):
+        for l in range(1 + p, len(matrix)):
             if matrix[p][p] != 0:
                 p_m = matrix[l][p] / matrix[p][p]
                 for c in range(len(matrix[l])):
                     matrix[l][c] = matrix[l][c] - p_m * matrix[p][c]
     return matrix
 
-def main():
-    
+
+matrix = []
+for i in range(int(input("quantas variaveis diferentes possuem os sistemas?: "))):
+    matrix.append(list(map(float, input("Digite os coeficientes separados por espaco: ").split())))
+for i in gauss_t(matrix): print(*i)
