868ca002fab8fa0f86d1657ba92133dca33be9b0
LukasHeidern <75574343+LukasHeidern@users.noreply.github.com>
2021-09-09 00:59:13 -0300

    Update Gauss.py

--- 2 Semestre/Algebra Linear e Geometria Analitica/Gauss.py
+++ 2 Semestre/Algebra Linear e Geometria Analitica/Gauss.py
@@ -1,15 +1,11 @@
-
-def gauss(matrix):
+def gauss_t(matrix):
     for p in range(len(matrix)-1):
         for l in range(1+p,len(matrix)):
             if matrix[p][p] != 0:
-                p_m = matrix[p+1][p] / matrix[p][p]
+                p_m = matrix[l][p] / matrix[p][p]
                 for c in range(len(matrix[l])):
                     matrix[l][c] = matrix[l][c] - p_m * matrix[p][c]
     return matrix
 
-matrix = []
-for i in range(int(input("Tamanho da matriz: "))):
-    matrix.append(list(map(float,input(f"Digite os elementos da linha {i+1} separados por espaco: ").split())))
-
-for i in gauss(matrix): print(*i)
+def main():
+    
